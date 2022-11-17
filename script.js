0449c5ed713110785028fa69c48cbb312386e7e9
ilias <ilias.fasilakhs@gmail.com>
2022-11-17 21:18:20 +0200

    working on JS

---
+++ script.js
@@ -0,0 +1,12 @@
+let form = document.querySelector('#quiz-form')
+let answerInputs = document.querySelectorAll('.answer')
+let questions = document.querySelector('.question-item')
+let alert = document.querySelector('#alert')
+
+
+form.addEventListener('submit', event=>{
+
+    event.preventDefault()
+    checkedAnswers = document.querySelector
+
+})
