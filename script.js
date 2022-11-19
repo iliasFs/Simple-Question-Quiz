d77a6500fbb2e89e06f3e9cbb668b001c6f840cb
ilias <ilias.fasilakhs@gmail.com>
2022-11-19 19:57:56 +0200

    Js working

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-let form = document.querySelector('#quiz-form')
-let answerInputs = document.querySelectorAll('.answer')
-let questions = document.querySelector('.question-item')
-let alert = document.querySelector('#alert')
+let form = document.querySelector("#quiz-form");
+let answerInputs = document.querySelectorAll(".answer");
+let questions = document.querySelectorAll(".question-item");
+let alert = document.querySelector("#alert");
+let checkedAnswers = [];
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  checkedAnswers = Array.from(answerInputs).filter((item) => {
+    return item.checked == true;
+  });
 
-form.addEventListener('submit', event=>{
-
-    event.preventDefault()
-    checkedAnswers = document.querySelector
-
-})
+  let rightAnswers = checkedAnswers.filter((item) => item.value == "true");
+  let wrongAnswers = checkedAnswers.filter((item) => item.value == "false");
+  rightAnswers.forEach((item) => questions.classList.add("correct"));
+  console.log(rightAnswers);
+});
